cae91b0b51640177dd9035a56bffd1cf7937adfb
tj-create <113359042+tj-create@users.noreply.github.com>
2024-03-14 18:40:40 +0900

    basic main

---
+++ src/main/java/hello/springbasic/SpringbasicApplication.java
@@ -0,0 +1,13 @@
+package hello.springbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/springbasic/SpringbasicApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
